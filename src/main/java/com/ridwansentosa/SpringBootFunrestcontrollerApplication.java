691e7132df63c94e29b2de4dc73365471ea5b2a6
ridwansentosa <mochridwansentosa@gmail.com>
2023-05-14 18:49:37 +0700

    frist commit

---
+++ src/main/java/com/ridwansentosa/SpringBootFunrestcontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.ridwansentosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFunrestcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFunrestcontrollerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ridwansentosa/SpringBootFunrestcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridwansentosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFunrestcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
